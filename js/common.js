29374681c5d48466e519d2b80bd3e2464d0ce870
Alexandr Pirs <alexandr.pirss@mail.ru>
2016-09-26 16:16:50 +0300

    two commit

--- js/common.js
+++ js/common.js
@@ -1,3 +1,14 @@
-$(".wrapper .top__line__header .search .search__button").click(function () {
-    $(".wrapper .top__line__header .search .search__input").fadeToggle("slow");
+$(function() {
+
+    $(".wrapper .top__line__header .search .search__button").click(function (e) {
+        $(".wrapper .top__line__header .search .search__input").fadeToggle("slow");
+        e.preventDefault()
+    });
+
+    $(".wrapper .top__line__header .mobile__icon").click(function (e) {
+        $(".wrapper .top__line__header .mobile__menu").slideToggle("slow");
+        e.preventDefault()
+    });
+
+
 });
